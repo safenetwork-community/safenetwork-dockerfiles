b4d1a6a6a48f8b475657751edfbca0d217dda62c
Folât Pjêrsômêj <folaht@protonmail.com>
2021-10-08 08:20:35 +0200

    test

--- scripts/install_safe.sh
+++ scripts/install_safe.sh
@@ -4,10 +4,17 @@ PLATFORM_ARCH=`uname -m`
 
 case "$PLATFORM_ARCH" in
   "armv7l")
-  PLATFORM_ARCH="armv7"
+  PLATFORM_ARCH="armv7-unknown-linux-musleabihf"
+  ;;
+  "armv6l")
+  PLATFORM_ARCH="arm-unknown-linux-musleabi"
+  ;;
+  *)
+  PLATFORM_ARCH=PLATFORM_ARCH+"-unknown-linux-musl"
   ;;
 esac
 
+# Add new paltform if unknown.
 echo $PLATFORM_ARCH
 
 # Create safe folders
@@ -16,7 +23,7 @@ mkdir -p ~/.safe/cli
 # Install the safe network command line interface
 curl -L $(curl --silent https://api.github.com/repos/maidsafe/sn_cli/releases/latest | \
   jq --arg PLATFORM_ARCH "$PLATFORM_ARCH" \
-  -r '.assets[] | select(.name | endswith($PLATFORM_ARCH+"-unknown-linux-musl.tar.gz")).browser_download_url') | \
+  -r '.assets[] | select(.name | endswith($PLATFORM_ARCH+".tar.gz")).browser_download_url') | \
   tar xz -C ~/.safe/
 
 # Make profile file with exported PATH and refresh the shell (while building)
