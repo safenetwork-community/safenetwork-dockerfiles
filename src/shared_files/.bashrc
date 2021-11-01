b1af3745c87adfe3c3b446649c28ba935bc2384f
Folât Pjêrsômêj <folaht@protonmail.com>
2021-11-01 21:03:03 +0100

    nodebin fix

--- src/shared_files/.bashrc
+++ src/shared_files/.bashrc
@@ -1,3 +1,3 @@
 alias vim=vi
 alias tb="nc termbin.com 9999"
-alias nodebin="~/.safe/sn_nodebin"
+alias nodebin="~/.safe/bin/sn_nodebin"
--- src/shared_files/.safe/bin/sn_nodebin.sh
+++ src/shared_files/.safe/bin/sn_nodebin.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-tac $(ls -t1 ~/.safe/node/root_dir/sn_node.log.* | head -n 1) \
-  | sed -e '/ERROR/{N;N;q}' | tac | tb
+root_dir=~/.safe/node/root_dir
+if [ -d "$root_dir" ]; then
+  log_file=$(ls -t1 $root_dir/sn_node.log* 2>/dev/null | head -n 1)
+  if [[ $log_file != ""]]; then
+    tac $log_file | sed -e '/ERROR/{N;N;q}' | tac | tb
+  else
+    echo -e "sn_node log not send\nMissing log file in directory $root_dir"
+    exit
+  fi
+else
+  echo -e "sn_node log not send\nMissing directory $root_dir"
+  exit
+fi
