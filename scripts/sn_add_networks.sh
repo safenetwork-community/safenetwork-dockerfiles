4ef3a2e89b1b30381642f179ae1fbef4d6d24816
Folât Pjêrsômêj <folaht@protonmail.com>
2022-01-15 16:02:51 +0100

    Added Dockerfiles

---
+++ scripts/sn_add_networks.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+safe networks add community-test https://jc-com-net.s3.eu-west-2.amazonaws.com/node_connection_info.config
---
+++ scripts/sn_install_latest.sh
@@ -0,0 +1,1 @@
+bash /tmp/sn_install.sh 0.52.13 0.50.6 0.43.2
