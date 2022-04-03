3f25ffffb1e431cf441b9bf68a95798c9320541f
Folât Pjêrsômêj <folaht@protonmail.com>
2022-04-04 01:10:54 +0200

    Added sjefolaht network

--- scripts/sn_add_networks.sh
+++ scripts/sn_add_networks.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
+# Company networks
+
 safe networks add community-test https://jc-com-net.s3.eu-west-2.amazonaws.com/node_connection_info.config
+
+
+# Community networks
+
+
+## Rezosur PRv4 d Folât
+
+safe networks add sjefolaht_ipv4 https://link.tardigradeshare.io/s/julx763rsy2egbnj2nixoahpobgq/rezosur/koqfig/sjefolaht_ipv4_node_connection_info.config?wrap=0
+
+## Rezosur PRv6 d Folât
+
+safe networks add sjefolaht https://link.tardigradeshare.io/s/julx763rsy2egbnj2nixoahpobgq/rezosur/koqfig/sjefolaht_node_connection_info.config?wrap=0
