daa531984bb26e6e577572d0a00eeabd8ba641d6
Folât Pjêrsômêj <folaht@protonmail.com>
2022-04-19 21:11:31 +0200

    keymappings location for Ubuntu #4

--- scripts/system_setup.sh
+++ scripts/system_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Nvim default lua files location
+# Nvim support files
 mkdir -p /usr/local/share/lua/5.1
-
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.config/nvim/keymappings.lua -o /usr/local/share/lua/5.1/keymappings.lua
