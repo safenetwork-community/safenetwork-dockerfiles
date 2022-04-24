eb36dbff95e5dcb760b8dc342d03a78bc0b747ca
Folât Pjêrsômêj <folaht@protonmail.com>
2022-04-24 06:14:09 +0200

    root|join merge attempt #2

--- scripts/user_setup.sh
+++ scripts/user_setup.sh
@@ -1,12 +1,12 @@
 # Add .bashrc and .bash_profile
-curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.bash_profile -o ~/.bash_profile
-curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.bashrc -o ~/.bashrc
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/src/shared_files/.bash_profile -o ~/.bash_profile
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/src/shared_files/.bashrc -o ~/.bashrc
 
 # Add safe network bin files
-curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.safe/bin/pb_nodes.sh -o ~/.safe/bin/pb_nodes.sh
-curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.safe/bin/pb_network.sh -o ~/.safe/bin/pb_network.sh
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/src/shared_files/.safe/bin/pb_nodes.sh -o ~/.safe/bin/pb_nodes.sh
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/src/shared_files/.safe/bin/pb_network.sh -o ~/.safe/bin/pb_network.sh
 chmod a+x ~/.safe/bin/*
 
 # Add nvim configuration files
 mkdir -p ~/.config/nvim
-curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.config/nvim/init.lua -o ~/.config/nvim/init.lua
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/src/shared_files/.config/nvim/init.lua -o ~/.config/nvim/init.lua
