6db41c8348703c6cda273ecb7bf42609aa759493
Folât Pjêrsômêj <folaht@protonmail.com>
2022-04-25 09:16:29 +0200

    root|join merge attempt #8

--- scripts/run_safenode.sh
+++ scripts/run_safenode.sh
@@ -1,29 +1,63 @@
 #!/bin/bash
 
-while getopts 'n:v:a:b:c:d:f:l:r:s:' c
+usage()
+{
+  echo "Usage: run_safenode [ -n | --name NETWORK_NAME ] 
+    [ -v | -vv | -vvv | -vvvv ]
+    [ -a | --con-ip CON_IP ] 
+    [ -b | --con-port CON_PORT ]"
+  exit 2
+}
+
+PARSED_ARGUMENTS=$(
+  getopt -a -n run_safenode \
+    --long name,verbose,con-ip,con-port,pub-ip,pub-port,first,\
+    idle-timeout-msec,keep-alive-interval-msec,log-dir,root-dir,\
+    skip-auto-port-forwarding: -- "$@")
+
+VALID_ARGUMENTS=$?
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage
+fi
+
+eval set -- "$PARSED_ARGUMENTS"
+while : 
 do
-  case $c in 
-    n | name) NETWORK_NAME=$OPTARG;;
-    v | verbose) VERBOSE=$OPTARG;;
-    a | con_ip) CON_IP=$OPTARG;;
-    b | con_port) CON_PORT=$OPTARG;;
-    c | pub_ip) PUB_IP=$OPTARG;;
-    d | pub_port) PUB_PORT=$OPTARG;;
-    f | first) FIRST=$OPTARG;;
-    i | idle_timeout_msec) IDLE_TIMEOUT_MSEC=$OPTARG;;
-    k | keep_alive_interval_msec) KEEP_ALIVE_INTERVAL_MSEC=$OPTARG;;
-    l | log_dir) LOG_DIR=$OPTARG;;
-    r | root_dir) ROOT_DIR=$OPTARG;;
-    s | skip-auto-port-forwarding) SKIP_AUTO_PORT_FOWARDING=$OPTARG;;
+  case "$1" in 
+    --name) NETWORK_NAME="$2"; shift 2;;
+    --verbose) VERBOSE="$2"; shift 2;;
+    --con_ip) CON_IP="$2"; shift 2;;
+    --con_port) CON_PORT="$2"; shift 2;;
+    --pub_ip) PUB_IP="$2"; shift 2;;
+    --pub_port) PUB_PORT="$2"; shift 2;;
+    --first) FIRST="$2"; shift 2;;
+    --idle_timeout_msec) IDLE_TIMEOUT_MSEC="$2"; shift 2;;
+    --keep_alive_interval_msec) KEEP_ALIVE_INTERVAL_MSEC="$2"; shift 2;;
+    --log_dir) LOG_DIR="$2"; shift 2;;
+    --root_dir) ROOT_DIR="$2"; shift 2;;
+    --skip-auto-port-forwarding) SKIP_AUTO_PORT_FOWARDING="$2"; shift 2;;
+    --) shift; break ;;
+    *) echo "Unexpected option: $1 - this should not happen."
+       usage ;;
   esac
 done
+
+if [ "$FIRST" = false ]; then 
+  safe network switch ${NETWORK_NAME}
+else
+  FIRST_C="--first"
+fi 
+
+if [ "$SKIP_AUTO_PORT_FOWARDING" = true ]; then
+  $SKIP_AUTO_PORT_FOWARDING_C=--skip-auto-port-forwarding
+fi
 
 RUST_BACKTRACE=full COLORBT_SHOW_HIDDEN=1 RUST_LOG=safe_network=error,qp2p=error sn_node ${VERBOSE} \
   --idle-timeout-msec ${IDLE_TIMEOUT_MSEC} \
   --keep-alive-interval-msec ${KEEP_ALIVE_INTERVAL_MSEC} \
-  if [ $SKIP_AUTO_PORT_FOWARDING = true ]; then --skip-auto-port-forwarding; fi \
+  ${SKIP_AUTO_PORT_FOWARDING} \
   --local-addr ${CON_IP}:${PUB_PORT} \
   --public-addr ${PUB_IP}:${PUB_PORT} \
   --log-dir ${LOG_DIR} \
   --root-dir ${ROOT_DIR} \
-  if [ $FIRST = true ]; then --first; fi
+  ${FIRST_C}
