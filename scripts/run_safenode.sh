f01345f096b96b1b0f1da311d98a8f4742f6defa
Folât Pjêrsômêj <folaht@protonmail.com>
2022-05-02 19:05:39 +0200

    4 Dockerfiles -> 3

--- scripts/run_safenode.sh
+++ scripts/run_safenode.sh
@@ -1,32 +1,62 @@
 #!/bin/bash
 
-usage()
+PROG_NAME=run_safenode
+
+SHORT_OPTS=hVv
+LONG_OPTS="name:,verbose:,num-nodes:,\
+skip-auto-port-forwarding:,\
+con-ip:,con-port:,pub-ip:,pub-port:,\
+idle-timeout-msec:,keep-alive-interval-msec:,\
+log-dir:,root-dir:,first:,version,help"
+
+IDLE_TIMEOUT_MSEC=4000
+KEEP_ALIVE_INTERVAL_MSEC=5500
+
+help()
 {
-  echo "Usage: run_safenode [ -n | --name NETWORK_NAME ] 
-    [ -v | -vv | -vvv | -vvvv ]
-    [ -a | --con-ip CON_IP ] 
-    [ -b | --con-port CON_PORT ]"
+  sn_node --help
+}
+
+version()
+{
+  echo "Podman run script for safenode v1.0"
   exit 2
 }
 
-PARSED_ARGUMENTS=$(
-  getopt -a -n run_safenode \
-    --long name,verbose,con-ip,con-port,pub-ip,pub-port,first,\
-    idle-timeout-msec,keep-alive-interval-msec,log-dir,root-dir,\
-    skip-auto-port-forwarding: -- "$@")
+usage()
+{
+  echo "Usage: run_safenode [ --name NETWORK_NAME ] 
+    [ --verbose VERBOSE ]
+    [ --num-nodes NUM_NODES ]
+    [ --skip-auto-port-forwarding SKIP_AUTO_PORT_FORWARDING ]
+    [ --idle-timeout-msec IDLE_TIMEOUT_MSEC ]
+    [ --keep-alive-interval-msec KEEP_ALIVE_INTERVAL_MSEC ]
+    [ --con-ip CON_IP ] 
+    [ --con-port CON_PORT ]
+    [ --pub-ip PUB_IP ]
+    [ --pub-port PUB_PORT ]
+    [ --log-dir LOG_DIR ]
+    [ --root-dir ROOT_DIR ]
+    [ --first FIRST ]"
+  exit 2
+}
+
+PARSED_ARGUMENTS=$(getopt -o $SHORT_OPTS --longoptions $LONG_OPTS --name $PROG_NAME -- "$@")
 
 VALID_ARGUMENTS=$?
 if [ "$VALID_ARGUMENTS" != "0" ]; then
   usage
 fi
 
+echo "PARSED_ARGUMENTS is $PARSED_ARGUMENTS"
 eval set -- "$PARSED_ARGUMENTS"
 while : 
 do
   case "$1" in 
     --name) NETWORK_NAME="$2"; shift 2;;
-    --verbose) VERBOSE="$2"; shift 2;;
-    --skip-auto-port-forwarding) SKIP_AUTO_PORT_FOWARDING="$2"; shift 2;;
+    -v | --verbose) VERBOSE+=v; shift ;;
+    --num-nodes) NUM_NODES="$2"; shift 2;;
+    --skip-auto-port-forwarding) SKIP_AUTO_PORT_FORWARDING="$2"; shift 2;;
     --idle-timeout-msec) IDLE_TIMEOUT_MSEC="$2"; shift 2;;
     --keep-alive-interval-msec) KEEP_ALIVE_INTERVAL_MSEC="$2"; shift 2;;
     --con-ip) CON_IP="$2"; shift 2;;
@@ -36,31 +66,84 @@ do
     --log-dir) LOG_DIR="$2"; shift 2;;
     --root-dir) ROOT_DIR="$2"; shift 2;;
     --first) FIRST="$2"; shift 2;;
+    -h | --help) help ;;
+    -V | --version) version ;;
     --) shift; break ;;
     *) echo "Unexpected option: $1 - this should not happen."
        usage ;;
   esac
 done
 
+echo "NETWORK_NAME : $NETWORK_NAME"
+echo "VERBOSE : $VERBOSE"
+echo "NUM_NODES : $NUM_NODES"
+echo "SKIP_AUTO_PORT_FORWARDING : $SKIP_AUTO_PORT_FORWARDING"
+echo "IDLE_TIMEOUT_MSEC : $IDLE_TIMEOUT_MSEC"
+echo "KEEP_ALIVE_INTERVAL_MSEC : $KEEP_ALIVE_INTERVAL_MSEC"
+echo "CON_IP : $CON_IP"
+echo "CON_PORT : $CON_PORT"
+echo "PUB_IP : $PUB_IP"
+echo "PUB_PORT : $PUB_PORT"
+echo "LOG_DIR : $LOG_DIR"
+echo "ROOT_DIR : $ROOT_DIR"
+echo "FIRST : $FIRST"
+
+if [ -n "$VERBOSE" ]; then
+  VERBOSE_OPT=" -${VERBOSE}"
+fi
+
+if [ "$SKIP_AUTO_PORT_FORWARDING" = true ]; then
+  SKIP_AUTO_PORT_FORWARDING_OPT=" --skip-auto-port-forwarding"
+fi
+
+if [ "$IDLE_TIMEOUT_MSEC" = true ]; then
+  IDLE_TIMEOUT_MSEC_OPT=" --idle-timeout-msec ${IDLE_TIMEOUT_MSEC}" 
+fi
+
+if [ "$KEEP_ALIVE_INTERVAL_MSEC" = true ]; then
+  KEEP_ALIVE_INTERVAL_MSEC_OPT=" --keep-alive-interval-msec ${KEEP_ALIVE_INTERVAL_MSEC}" 
+fi
+
+if [ -n "$LOG_DIR" ]; then
+  LOG_DIR_OPT=" --log-dir ${LOG_DIR}" 
+fi
+
+if [ -n "$ROOT_DIR" ]; then
+  ROOT_DIR_OPT=" --root-dir ${ROOT_DIR}" 
+fi
+
 if [ "$FIRST" = false ]; then
   safe networks switch ${NETWORK_NAME}
 else
-  FIRST_C=--first
+  FIRST_OPT=" --first"
 fi 
 
-if [ "$SKIP_AUTO_PORT_FOWARDING" = true ]; then
-  $SKIP_AUTO_PORT_FOWARDING_C=--skip-auto-port-forwarding
-fi
+CON_OPT=" --local-addr ${CON_IP}:${CON_PORT}"
+PUB_OPT=" --public-addr ${PUB_IP}:${PUB_PORT}"
 
-RUST_BACKTRACE=full COLORBT_SHOW_HIDDEN=1 RUST_LOG=safe_network=error,qp2p=error sn_node ${VERBOSE} \
-  --idle-timeout-msec ${IDLE_TIMEOUT_MSEC} \
-  --keep-alive-interval-msec ${KEEP_ALIVE_INTERVAL_MSEC} \
-  ${SKIP_AUTO_PORT_FOWARDING_C} \
-  --local-addr ${CON_IP}:${PUB_PORT} \
-  --public-addr ${PUB_IP}:${PUB_PORT} \
-  --log-dir ${LOG_DIR} \
-  --root-dir ${ROOT_DIR} \
-  ${FIRST_C}
+echo RUST_BACKTRACE=full COLORBT_SHOW_HIDDEN=1 RUST_LOG=safe_network=error,qp2p=error sn_node\
+${VERBOSE_OPT}${IDLE_TIMEOUT_MSEC_OPT}${KEEP_ALIVE_INTERVAL_MSEC_OPT}${SKIP_AUTO_PORT_FORWARDING_OPT}\
+${CON_OPT}${PUB_OPT}${LOG_DIR_OPT}${ROOT_DIR_OPT}\
+${FIRST_OPT}
+
+RUST_BACKTRACE=full COLORBT_SHOW_HIDDEN=1 RUST_LOG=safe_network=error,qp2p=error sn_node\
+${VERBOSE_OPT}${IDLE_TIMEOUT_MSEC_OPT}${KEEP_ALIVE_INTERVAL_MSEC_OPT}${SKIP_AUTO_PORT_FORWARDING_OPT}\
+${CON_OPT}${PUB_OPT}${LOG_DIR_OPT}${ROOT_DIR_OPT}${FIRST_OPT}
+
+for (( i = 1; i <= NUM_NODES; i++ ))
+  do
+  CON_PORT=$(($CON_PORT + $i))
+  PUB_PORT=$(($PUB_PORT + $i))
+
+  echo RUST_BACKTRACE=full COLORBT_SHOW_HIDDEN=1 RUST_LOG=safe_network=error,qp2p=error sn_node\
+${VERBOSE_OPT}${IDLE_TIMEOUT_MSEC_OPT}${KEEP_ALIVE_INTERVAL_MSEC_OPT}${SKIP_AUTO_PORT_FORWARDING_OPT}\
+${CON_OPT}${PUB_OPT}${LOG_DIR_OPT}${ROOT_DIR_OPT}
+
+  RUST_BACKTRACE=full COLORBT_SHOW_HIDDEN=1 RUST_LOG=safe_network=error,qp2p=error sn_node\
+${VERBOSE_OPT}${IDLE_TIMEOUT_MSEC_OPT}${KEEP_ALIVE_INTERVAL_MSEC_OPT}${SKIP_AUTO_PORT_FORWARDING_OPT}\
+${CON_OPT}${PUB_OPT}${LOG_DIR_OPT}${ROOT_DIR_OPT}
+
+  done
 
 if [ "$FIRST" = true ]; then 
   safe networks add ${NETWORK_NAME}
--- scripts/test/joinnode_example.sh
+++ scripts/test/joinnode_example.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-./run_safenode.sh --name test 
+../run_safenode.sh 
+  --name join_test \
+  --verbose -vv \
+  --skip-auto-port-forwarding true \
+  --idle-timeout-msec 5500 \
+  --keep-alive-interval-msec 4000 \
+  --con-ip 10.88.0.2 \
+  --con-port 12000 \
+  --pub-ip 195.240.113.178 \
+  --pub-port 12000 \
+  --log-dir ~/.safe/node/node_dir_0\
+  --root-dir ~/.safe/node/node_dir_0\
