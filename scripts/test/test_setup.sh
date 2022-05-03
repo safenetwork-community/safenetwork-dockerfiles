0e0caec5180bc5f91b885fcd4b3bea273bc53142
Folât Pjêrsômêj <folaht@protonmail.com>
2022-05-03 05:51:50 +0200

    3 Dockerfiles -> 2 #1

--- scripts/test/test_setup.sh
+++ scripts/test/test_setup.sh
@@ -4,7 +4,6 @@
 mkdir -p ~/.safe/test
 
 # Test files
-curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/scripts/test/run_safenode.sh -o ~/.safe/test/run_safenode.sh
 curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/scripts/test/rootnode_example.sh -o ~/.safe/test/rootnode_example.sh
 curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/scripts/test/joinnode_example.sh -o ~/.safe/test/joinnode_example.sh
 chmod a+x ~/.safe/bin/*
