65ef727deaf1c0fbc75d0ba8ab32261afbbc66e3
Folât Pjêrsômêj <folaht@protonmail.com>
2022-05-18 10:28:29 +0200

    nvim keymappings bugfix

--- scripts/system_setup.sh
+++ scripts/system_setup.sh
@@ -2,4 +2,4 @@
 
 # Nvim support files
 mkdir -p /usr/local/share/lua/5.1
-curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/src/shared_files/.config/nvim/keymappings.lua -o /usr/local/share/lua/5.1/keymappings.lua
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/ubuntu/src/shared_files/.config/nvim/lua/keymappings.lua -o /usr/local/share/lua/5.1/keymappings.lua
