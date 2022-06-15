15ec8f8840ca480ca71a8a3c9257472f06279a83
Folât Pjêrsômêj <folaht@protonmail.com>
2022-06-15 15:20:07 +0200

    resolved /var/logs bug

--- scripts/system_setup.sh
+++ scripts/system_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Safe network node log directory
+install -d -o root -g admin -m 750 /var/log/sn_node
+
 # Nvim support files
 mkdir -p /usr/local/share/lua/5.1
 curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.config/nvim/lua/keymappings.lua -o /usr/local/share/lua/5.1/keymappings.lua
+
+
