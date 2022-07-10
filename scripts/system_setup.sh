ffe2827594cf19544e72acfdd6589c03db6f3085
Folât Pjêrsômêj <folaht@protonmail.com>
2022-07-10 22:12:43 +0200

    nvim root

--- scripts/system_setup.sh
+++ scripts/system_setup.sh
@@ -4,7 +4,8 @@
 install -d -o root -g admin -m 750 /var/log/sn_node
 
 # Nvim support files
+mkdir -p ~/.config/nvim
 mkdir -p /usr/local/share/lua/5.1
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.config/nvim/init.lua -o ~/.config/nvim/init.lua
 curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.config/nvim/lua/keymappings.lua -o /usr/local/share/lua/5.1/keymappings.lua
-
-
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.config/nvim/lua/keymappings.lua -o /usr/share/lua/5.1/keymappings.lua
--- scripts/user_setup.sh
+++ scripts/user_setup.sh
@@ -16,6 +16,6 @@ curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dock
 curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.safe/test/joinnode_example.sh -o ~/.safe/test/joinnode_example.sh
 chmod a+x ~/.safe/test/*
 
-# Add nvim configuration files
+# Nvim support files
 mkdir -p ~/.config/nvim
 curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.config/nvim/init.lua -o ~/.config/nvim/init.lua
