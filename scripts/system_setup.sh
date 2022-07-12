398895de8fa8acd034cfd724085b397b9418ffbd
Folât Pjêrsômêj <folaht@protonmail.com>
2022-07-12 02:50:52 +0200

    .bashrc root

--- scripts/system_setup.sh
+++ scripts/system_setup.sh
@@ -3,6 +3,10 @@
 # Safe network node log directory
 install -d -o root -g admin -m 750 /var/log/sn_node
 
+# Add .bashrc and .bash_profile
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.bash_profile -o ~/.bash_profile
+curl -s https://raw.githubusercontent.com/safenetwork-community/safenetwork-dockerfiles/latest/src/shared_files/.bashrc -o ~/.bashrc
+
 # Nvim support files
 mkdir -p ~/.config/nvim
 mkdir -p /usr/local/share/lua/5.1
