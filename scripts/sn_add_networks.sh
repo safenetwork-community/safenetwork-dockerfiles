f987aefec9e5c79fe262e050f4396aa94ee7aee3
Folât Pjêrsômêj <folaht@protonmail.com>
2022-07-12 16:19:40 +0200

    comnets update

--- scripts/sn_add_networks.sh
+++ scripts/sn_add_networks.sh
@@ -9,8 +9,8 @@
 
 ## Rezosur PRv4 d Folât
 
-safe networks add sjefolaht_ipv4 https://link.tardigradeshare.io/s/julx763rsy2egbnj2nixoahpobgq/rezosur/koqfig/sjefolaht_ipv4_node_connection_info.config?wrap=0
+safe networks add sjefolaht_ipv4 https://link.tardigradeshare.io/s/julx763rsy2egbnj2nixoahpobgq/rezosur/koqfig/klik_sjefolaht_ipv4?wrap=0
 
 ## Rezosur PRv6 d Folât
 
-safe networks add sjefolaht https://link.tardigradeshare.io/s/julx763rsy2egbnj2nixoahpobgq/rezosur/koqfig/sjefolaht_node_connection_info.config?wrap=0
+safe networks add sjefolaht https://link.tardigradeshare.io/s/julx763rsy2egbnj2nixoahpobgq/rezosur/koqfig/klik_sjefolaht.config?wrap=0
